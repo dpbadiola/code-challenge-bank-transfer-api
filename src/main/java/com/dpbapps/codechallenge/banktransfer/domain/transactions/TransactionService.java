2610797a0f55a385c236074b832b3c3acc3e91db
David Badiola <dpebadiola@gmail.com>
2025-01-11 23:12:03 +0800

    complete get transactions logic

--- src/main/java/com/dpbapps/codechallenge/banktransfer/domain/transactions/TransactionService.java
+++ src/main/java/com/dpbapps/codechallenge/banktransfer/domain/transactions/TransactionService.java
@@ -1,30 +1,42 @@
 package com.dpbapps.codechallenge.banktransfer.domain.transactions;
 
+import com.dpbapps.codechallenge.banktransfer.domain.error.exceptions.RecordNotFoundException;
 import com.dpbapps.codechallenge.banktransfer.domain.transactions.dto.TransactionRequest;
 import com.dpbapps.codechallenge.banktransfer.domain.transactions.dto.TransactionResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
-import java.time.Instant;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionService {
 
+	private final TransactionsRepository repository;
+
+	@Transactional(readOnly = true)
 	public TransactionResponse getTransaction(UUID uuid) {
-		return TransactionResponse.builder()
-			.uuid(UUID.randomUUID())
-			.transactionTimestamp(Instant.now())
-			.postingTimestamp(Instant.now())
-			.amount(BigDecimal.ONE)
-			.transferFee(BigDecimal.ZERO)
-			.endingBalance(BigDecimal.TEN)
-			.build()
+		return repository.findById(uuid)
+			.map(this::mapTransactions)
+			.orElseThrow(() -> new RecordNotFoundException("Transaction not found"))
 			;
 	}
 
 	public UUID createTransaction(TransactionRequest request) {
 		return UUID.randomUUID();
+	}
+
+	private TransactionResponse mapTransactions(Transactions entity) {
+		return TransactionResponse.builder()
+			.uuid(entity.getUuid())
+			.transactionTimestamp(entity.getCreatedDate())
+			.postingTimestamp(entity.getPostingDate())
+			.amount(entity.getCredit().subtract(entity.getDebit()))
+			.transferFee(entity.getTransferFee())
+			.endingBalance(entity.getEndingBalance())
+			.build()
+			;
 	}
 
 }
